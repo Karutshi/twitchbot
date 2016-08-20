9fc38060c7f6edf842c631ac74844d93604fda1e
Johan Ribom <jribom@kth.se>
2016-08-21 01:32:09 +0200

    Added support for moderators and staff in chat.

--- twitchbot/twitchbot.py
+++ twitchbot/twitchbot.py
@@ -1,4 +1,5 @@
 import socket, string, threading, time, re
+import urllib2, json, readline 
 
 class Twitchbot:
 
@@ -20,27 +21,48 @@ class Twitchbot:
         self.readbuffer = ""
         self.MODT = False
         self.channel = raw_input(self.Color.FAIL + "Enter the channel name to connect to.\r\n" + self.Color.ENDC)
+        self.printColor(self.Color.FAIL, "Connecting to http://www.twitch.tv/" + self.channel)
+       
+        try:
+            self.s = socket.socket()
+            self.s.connect((self.HOST, self.PORT))
+            self.s.send("PASS " + self.PASS + "\r\n")
+            self.s.send("NICK " + self.NICK + "\r\n")
+            self.s.send("JOIN #" + self.channel + " \r\n")
+            self.keepReading = True
+            self.biglist = []
+            self.pause = False
+            response = urllib2.urlopen("https://tmi.twitch.tv/group/user/" + self.channel + "/chatters")
+            html = response.read()
+            parsed_json = json.loads(html)
+            self.users = parsed_json.get("chatters")
+            self.printColor(self.Color.OKGREEN, "Connected!")
+        except Exception as e:
+            print "Could not connect to http://www.twitch.tv/" + self.channel
+            print e
+    def checkuser(self, name):
+        for key in self.users:
+            if name in self.users.get(key):
+                self.printColor(self.Color.OKGREEN, "User '" + name + "' is currently watching and is a member of group '" + key + "'.")
+                break
+        else:
+            self.printColor(self.Color.OKGREEN, "User '" + name + "' is not currently watching.")
 
-        self.s = socket.socket()
-        self.s.connect((self.HOST, self.PORT))
-        self.s.send("PASS " + self.PASS + "\r\n")
-        self.s.send("NICK " + self.NICK + "\r\n")
-        self.s.send("JOIN #" + self.channel + " \r\n")
-        print self.Color.FAIL + "Connecting to http://www.twitch.tv/" + self.channel + self.Color.ENDC
-        self.keepReading = True
-        self.biglist = []
-        self.pause = False
+    def printColor(self, color, message):
+        print color + message + self.Color.ENDC
 
     def Send_message(self, message):
-        self.s.send("PRIVMSG #" + self.channel + " :" + message + "\r\n")
-        print self.Color.OKBLUE + "karutshi: " + message + self.Color.ENDC
         self.pause = False
+        if message == "":
+            return
+        self.s.send("PRIVMSG #" + self.channel + " :" + message + "\r\n")
+        self.printColor(self.Color.OKBLUE, "karutshi: " + message)
 
     def Change_channel(self, newchannel):
         self.s.send("PART #" + self.channel + " \r\n")
         self.channel = newchannel
         self.s.send("JOIN #" + self.channel + " \r\n")
-        print self.Color.FAIL + "Connecting to http://www.twitch.tv/" + self.channel + self.Color.ENDC
+        self.printColor(self.Color.FAIL, "Connecting to http://www.twitch.tv/" + self.channel)
 
         self.pause = False
 
@@ -83,19 +105,19 @@ class Twitchbot:
 
                                 if self.MODT:
                                     color = ""
-                                    if re.search("bot$", username):
+                                    if username in self.users.get("moderators") or username in self.users.get("staff"):
                                         color = self.Color.FAIL
                                     elif username.lower() == "karutshi":
                                         color = self.Color.OKBLUE
                                     elif "karutshi" in message or re.search("[\s:]karu[\s\,\.\!]", username, re.I):
                                         color = self.Color.WARNING 
-                                    print color + username + ":" + message + self.Color.ENDC 
+                                    self.printColor(color, username + ":" + message) 
 
                                 for l in parts:
                                     if "End of /NAMES list" in l:
                                         self.MODT = True
                             elif "PART" in parts[1]:
-                                print self.Color.FAIL + ":".join(parts) + self.Color.ENDC
+                                self.printColor(self.Color.FAIL, ":".join(parts))
                         except:
                             for l in parts:
                                 print l
@@ -115,5 +137,7 @@ while True:
     elif command.lower() == "change":
         twitchbot.Pause()
         twitchbot.Change_channel(raw_input(twitchbot.Color.FAIL + "Enter new channel name. \r\n" + twitchbot.Color.ENDC))
+    elif command.lower() == "checkuser":
+        twitchbot.checkuser(raw_input("Enter the username.\n"))
     else:
         twitchbot.Send_message(command)
