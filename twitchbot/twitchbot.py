99ed0ab13bf5f1f7285f36de9621534748a4f70d
Johan Ribom <jribom@kth.se>
2017-02-25 12:29:13 +0100

    :bug: Fix the check for PING from twitch

--- twitchbot/twitchbot.py
+++ twitchbot/twitchbot.py
@@ -235,11 +235,11 @@ class Twitchbot:
             for line in temp:
                 # Respond to twitch pings.
                 print line
-                if (line[0] == "PING"):
-                    self.s.send("PONG %s\r\n" % line[1])
-                    print "PONG %s\r\n" % line[1]
+                parts = string.split(line, ":")
+                if "PING" in parts[0]:
+                    self.s.send("PONG %s\r\n" % parts[1])
+                    print "PONG %s\r\n" % parts[1]
                 else:
-                    parts = string.split(line, ":")
                     try:
                         if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
                             try:
